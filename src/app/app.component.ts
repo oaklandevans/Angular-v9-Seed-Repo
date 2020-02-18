b46996e3aa11fd849622886b06b2029ef0845e68
kevinruse <kevin@kevinruse.com>
2020-02-18 12:40:44 -0800

    add header component per challenge exercise 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: `<div><p>The app is working!</p></div>`
+    template: `<div>
+                    <app-header></app-header>
+                    <p>The app is working!</p>
+               </div>`
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, HeaderComponent ],
     bootstrap:    [ AppComponent ]
 })
 
