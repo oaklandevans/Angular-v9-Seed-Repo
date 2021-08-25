14366a2404ae56ff509ae41967503b196ece252d
Oakland Evans <oaklandevans@yahoo.com>
2021-08-25 17:05:15 -0500

    ngStyle directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,28 @@ export class AppComponent {
     user = {
         id: 1, name: 'Kevin', gender: 'M', ageGroup: '51+',
         userCode: 'M51+', reqs: {}, reqsStatus: {
-        fruitMet: false, vegMet: false,
-        proteinMet: false, grainMet: false
+            fruitMet: false, vegMet: false,
+            proteinMet: false, grainMet: false
         }, registered: true, email: 'kevin@kevinruse.com'
     };
 
     loggedIn: boolean = false;
+    stylePreference: string;
 
     logIn(evt) {
         console.log(evt);
         this.loggedIn = evt;
+    }
+
+    setStylePreference(value) {
+        this.stylePreference = value;
+    }
+
+    setImportance() {
+        if (this.stylePreference === 'hilite') {
+            return { 'background-color': 'yellow' };
+        } else if (this.stylePreference === 'caps') {
+            return { 'text-transform': 'uppercase' };
+        }
     }
 }
